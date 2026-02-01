c71dcafd9273b54d8fccb21ed1966249346728de
alexandre <alexandre020602@gmail.com>
2026-02-01 02:02:28 -0300

    import header and gradient div

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Header from "./components/Header/Header"
 
 
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <main>
+      <img className="absolute top-0 right-0 opacity-60 -z-1" src="/gradient.png" alt="gradient-img" />
+      <div className="h-0 w-160 absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-30 -z-10"></div>
+
+      <Header />
+    </main>
   )
 }
 
